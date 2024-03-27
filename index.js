f345ed9c53e016cd66cdd484d351546b96ceb19c
Tatiana Mathia <mathiatatiana@gmail.com>
2024-03-27 13:03:38 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,15 @@
+const num1 = 2000
+const num2 = 100
+function divide (num1,num2){
+   
+    return (num1/num2)
+}
+
+let num3 = 2
+const square=(num3)=>{
+   return num3*num3}
+
+let num4 = 1
+let num5 = 3
+const add=(num4,num5)=>{
+    return num4+num5}
